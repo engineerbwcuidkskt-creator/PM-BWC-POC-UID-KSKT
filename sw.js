8da4dfaca71d321848fa59ff9ce111ce57d6c90f
engineerbwcuidkskt-creator <engineer.bwc.uid.kskt@gmail.com>
2026-08-28 10:29:21 +0800

    Perbaiki cache agar dashboard selalu terbaru

--- sw.js
+++ sw.js
@@ -12,5 +12,8 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+  const isAppAsset = event.request.mode === 'navigate' || /\.(html|js|css)$/.test(new URL(event.request.url).pathname);
+  event.respondWith(isAppAsset
+    ? fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match(event.request))
+    : caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
