02a68dd4dab20c5beb878e5516e1ba8f2507267d
engineerbwcuidkskt-creator <engineer.bwc.uid.kskt@gmail.com>
2026-08-28 09:37:15 +0800

    Create master-data.js

---
+++ data/master-data.js
@@ -0,0 +1,21 @@
+const masterData = {
+  up3Options: [
+    'UP3 Banjarmasin',
+    'UP3 Kuala Kapuas',
+    'UP3 Palangkaraya',
+    'UP3 Barabai',
+    'UP3 Batulicin',
+    'UP3 Pangkalan Bun'
+  ],
+  ulpOptions: {
+    'UP3 Banjarmasin': ['ULP Banjarmasin Kota', 'ULP Banjarmasin Selatan', 'ULP Banjarbaru', 'ULP Martapura', 'ULP Marabahan'],
+    'UP3 Kuala Kapuas': ['ULP Kuala Kapuas', 'ULP Pulang Pisau', 'ULP Tamiang Layang'],
+    'UP3 Palangkaraya': ['ULP Palangkaraya', 'ULP Kasongan', 'ULP Kuala Kurun', 'ULP Tewah'],
+    'UP3 Barabai': ['ULP Barabai', 'ULP Kandangan', 'ULP Rantau', 'ULP Negara'],
+    'UP3 Batulicin': ['ULP Batulicin', 'ULP Kotabaru', 'ULP Pelaihari'],
+    'UP3 Pangkalan Bun': ['ULP Pangkalan Bun', 'ULP Kumai', 'ULP Sukamara', 'ULP Lamandau']
+  },
+  deviceTypes: ['BWC Hytera', 'POC Hytera'],
+  deviceModels: { 'BWC Hytera': ['Hytera SC580'], 'POC Hytera': ['Hytera PNC380'] },
+  statuses: ['Terjadwal', 'Dalam Proses', 'Selesai', 'Terlambat']
+};
